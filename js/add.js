f4e593c589edd67239eb75a5aadea34d71cf7717
FApew <112774976+FApew@users.noreply.github.com>
2024-05-14 17:07:56 +0200

    addedCPR

--- js/add.js
+++ js/add.js
@@ -1,9 +1,20 @@
-//link che portano alle altre pagine del sito dal main
+import { arrPr } from "./assets.js"
 
+//link che portano alle altre pagine del sito dal main
 const Link1 = document.getElementById("tab1")
 const Link2 = document.getElementById("tab2")
 const Link3 = document.getElementById("tab3")
 const Link4 = document.getElementById("tab4")
+
+//funzione di click alle freccine 
+document.getElementById("dxPr").addEventListener("click", () => {
+    changePr(1)
+})
+document.getElementById("sxPr").addEventListener("click", () => {
+    changePr(-1)
+})
+
+let cPr = 0
 
 Link1.addEventListener("mousedown", ()=> {
     window.open("./page1.html")
@@ -17,3 +28,18 @@ Link3.addEventListener("mousedown", ()=> {
 Link4.addEventListener("mousedown", ()=> {
     window.open("./page4.html")
 })
+
+//aggiornamento schermo proccessori con le freccine
+function changePr(n) {
+    //cambia e normalizza cPr
+    cPr+=n
+    if (cPr <= 0) {
+        cPr = arrPr.length - 1
+    } else if (cPr >= arrPr.length) {
+        cPr = 0
+    }
+
+    //Metti info dentro al rtettangolo
+    console.log(arrPr[cPr].name)
+}
+
---
+++ js/assets.js
@@ -0,0 +1,8 @@
+export let arrPr = [{
+    name: "nome",
+    nCore: "2⁶³",
+    speed: "brooom",
+    arch: "villa al lago",
+    threads: "tipo troppi bro"
+
+}]
