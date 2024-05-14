522d9a607d0f58d5ab6d4b97b97b4a010169b945
fffppp-pixel <francype06@gmail.com>
2024-05-15 00:35:42 +0200

    lastcommit

--- js/add.js
+++ js/add.js
@@ -1,30 +1,46 @@
-import { arrPr } from "./assets.js"
+let arrPr = [{
+    name: "nome",
+    nCore: "2⁶³",
+    speed: "brooom",
+    arch: "villa al lago",
+    threads: "tipo troppi bro"
 
+}
+,
+{
+    name: "nome2",
+    nCore: "23⁶³",
+    speed: "brooom2",
+    arch: "villa al lago2",
+    threads: "tipo troppi bro2"
 
+}
+,
+{
+    name: "nome2",
+    nCore: "24⁶³",
+    speed: "brooom3",
+    arch: "villa al lago3",
+    threads: "tipo troppi bro3"
+
+}
+]
+
+const LDXB = document.getElementById("sxPL")
+const LSXB = document.getElementById("dxPL")
 
 //funzione di click alle freccine 
-document.getElementById("dxPr").addEventListener("click", () => {
-    changePr(1)
+LDXB.addEventListener("mousedown", () => {
+    changestat(1)
+    alert("dx")
 })
-document.getElementById("sxPr").addEventListener("click", () => {
-    changePr(-1)
+LSXB.addEventListener("mousedown", () => {
+    changestat(1)
+    alert("sx")
 })
 
-let cPr = 0
 
 
 
-//aggiornamento schermo proccessori con le freccine
-function changePr(n) {
-    //cambia e normalizza cPr
-    cPr+=n
-    if (cPr <= 0) {
-        cPr = arrPr.length - 1
-    } else if (cPr >= arrPr.length) {
-        cPr = 0
-    }
 
-    //Metti info dentro al rtettangolo
-    console.log(arrPr[cPr].name)
-}
 
--- js/assets.js
+++
@@ -1,8 +0,0 @@
-export let arrPr = [{
-    name: "nome",
-    nCore: "2⁶³",
-    speed: "brooom",
-    arch: "villa al lago",
-    threads: "tipo troppi bro"
-
-}]
